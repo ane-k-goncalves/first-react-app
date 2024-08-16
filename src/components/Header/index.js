4f032f6f347c146a71131dcec5eeea601938f1fd
ane-k-goncalves <goncalvesanne961@gmail.com>
2024-08-16 17:54:44 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <header>
+            <span>My First APP React JS </span>
+            <nav>
+                <a href="#"> Home</a>
+                <a href="#"> Sobre</a>
+
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
